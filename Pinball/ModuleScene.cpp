91602d13fd5f24a02ca67cad75392f26eb7f8c30
Dídac Pérez Márquez <125640145+didacpema@users.noreply.github.com>
2023-11-19 21:40:08 +0100

    a

--- Pinball/ModuleScene.cpp
+++ Pinball/ModuleScene.cpp
@@ -66,6 +66,7 @@ bool ModuleScene::Start()
 
 	flipperFx = App->audio->LoadFx("pinball/Audio/flipper.ogg");
 	music = App->audio->LoadFx("pinball/Audio/music.ogg");
+	combo = App->audio->LoadFx("pinball/Audio/combo.ogg");
 
 	arrowLights.PushBack({ 0,0,480,800 });
 	arrowLights.PushBack({ 480,0,480,800 });
@@ -584,6 +585,7 @@ update_status ModuleScene::Update()
 		piston1Enabled = false;
 		piston2Enabled = false;
 		piston3Enabled = false;
+		App->audio->PlayFx(combo);
 	}
 
 	if (piston4Enabled && piston5Enabled && piston6Enabled)
@@ -592,6 +594,7 @@ update_status ModuleScene::Update()
 		piston4Enabled = false;
 		piston5Enabled = false;
 		piston6Enabled = false;
+		App->audio->PlayFx(combo);
 	}
 
 	if (!piston1Enabled)
